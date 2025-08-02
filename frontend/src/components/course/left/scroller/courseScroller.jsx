be4e4b3625db94fd8bb31bdada76e3362e79ade7
Alexander Artemev <avartemev_1@edu.hse.ru>
2025-08-03 02:49:09 +0500

    Merge pull request #1
    * feat(blocks): подключена кнопка добавления блока к API
    
    * feat(topics): подключена кнопка добавления темы к API

--- frontend/src/components/course/left/scroller/courseScroller.jsx
+++ frontend/src/components/course/left/scroller/courseScroller.jsx
@@ -1,12 +1,12 @@
-import {
-    SidebarGroup,
-    SidebarMenu, SidebarMenuItem,
-} from "@/components/ui/sidebar.jsx";
-
-import { TopicItem } from "./TopicItem.jsx";
+import {SidebarGroup, SidebarMenu, SidebarMenuItem} from "@/components/ui/sidebar.jsx";
+import {TopicItem} from "./TopicItem.jsx";
 import {AddTopicButton} from "@/components/course/left/scroller/editButton/addTopicButton.jsx";
+import {useCourseMutations} from "@/features/useCourseMutations.js";
+import {useParams} from "react-router-dom";
 
-export function CourseScroller({ items, onEditTopic, onAddBlock }) {
+export function CourseScroller({items, onEditTopic, onAddBlock}) {
+    const {courseId} = useParams();
+    const {addTopic} = useCourseMutations(courseId);
 
     return (
         <SidebarGroup>
@@ -20,9 +20,10 @@ export function CourseScroller({ items, onEditTopic, onAddBlock }) {
                     />
                 ))}
                 <SidebarMenuItem>
-                    <AddTopicButton onClick={()=>1} />
+                    <AddTopicButton onClick={() => {
+                        addTopic.mutate();
+                    }}/>
                 </SidebarMenuItem>
-
             </SidebarMenu>
         </SidebarGroup>
     );
